b3eea81c2f38a767cf15c73b1b4daf3803552cd1
felipestollbr-cmd <felipestollbr@gmail.com>
2026-01-19 19:06:16 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,23 @@
-const CACHE_NAME = 'eduplay-v1';
-const assets = ['index.html', 'https://cdn.tailwindcss.com'];
+const CACHE_NAME = 'eduplay-v2';
+const assets = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
